6bf499b1552075248a538e5aab1f9702c92f8331
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-26 05:11:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,12 +46,11 @@ def reduce_to_all_true(array)
   i = 0
   while i < array.length do
     if array[i] == false
-      return true
-    else
-      return false
+      array[i] = true
     end
     i += 1
   end
+  true
 end
 
 def reduce_to_any_true(array)
